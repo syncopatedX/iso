10f7fd0e6885730ffcc8f0e61c6244bedb48049a
Robert Pannick <rwpannick@gmail.com>
2025-05-29 01:54:37 -0400

    chore: Renamed installer to installer.sh

--- profiledef.sh
+++ profiledef.sh
@@ -37,5 +37,5 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/installer"]="0:0:755"
+  ["/usr/local/bin/installer.sh"]="0:0:755"
 )
